81837018cbae085caf6d8b7dfb91876a8b7d3d64
John DeMastri <demastri@msoe.edu>
2023-10-22 20:56:07 -0500

    Chapter 24 - page 269 after listing 24.2 - added main activity, custom BoxDrawingView, updated theme and colors - not mentioned in book - to match their presentation (app bar and purple overall color)

---
+++ app/src/main/java/edu/msoe/demastri/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,12 @@
+package edu.msoe.demastri.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+}
