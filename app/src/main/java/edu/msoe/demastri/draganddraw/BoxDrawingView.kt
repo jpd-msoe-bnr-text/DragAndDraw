8ae929a9d04c52b9bd39cd621b56b5951a4abf4f
John DeMastri <demastri@msoe.edu>
2023-10-22 21:07:50 -0500

    Chapter 24 - page 271 after listing 24.7 - added a Box data class to capture draw constraints, Paint references to draw, and overrode onDraw to show boxes as dragged/drawn

--- app/src/main/java/edu/msoe/demastri/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/edu/msoe/demastri/draganddraw/BoxDrawingView.kt
@@ -1,6 +1,8 @@
 package edu.msoe.demastri.draganddraw
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
 import android.util.Log
@@ -14,21 +16,38 @@ class BoxDrawingView(
     attrs: AttributeSet? = null
 ) : View(context, attrs) {
 
+    private var currentBox: Box? = null
+    private val boxes = mutableListOf<Box>()
+
+    private val boxPaint = Paint().apply {
+        color = 0x22ff0000.toInt()
+    }
+    private val backgroundPaint = Paint().apply {
+        color = 0xfff8efe0.toInt()
+    }
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 action = "ACTION_DOWN"
+                // Reset drawing state
+                currentBox = Box(current).also {
+                    boxes.add(it)
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
+                updateCurrentBox(current)
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
+                currentBox = null
             }
         }
 
@@ -37,4 +56,19 @@ class BoxDrawingView(
         return true
     }
 
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        // Fill the background
+        canvas.drawPaint(backgroundPaint)
+
+        boxes.forEach { box ->
+            canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
+        }
+    }
 }
